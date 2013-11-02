383602fd52d900b196ce83ec3d18f2927205b767
Cary WR Campbell <cwrc@xpasc.com>
2013-11-01 23:44:10 -0400

    Include changes from 5.6 Exercises.

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,8 +1,11 @@
-def full_title(page_title)
-  base_title = "RRTGen RESTful Executive Dashboard"
-  if page_title.empty?
-    base_title
-  else
-    "#{base_title} | #{page_title}"
-  end
-end
+include ApplicationHelper
+
+# ApplicationHelper replaces the full_title helper:
+#def full_title(page_title)
+#  base_title = "RRTGen RESTful Executive Dashboard"
+#  if page_title.empty?
+#    base_title
+#  else
+#    "#{base_title} | #{page_title}"
+#  end
+#end
